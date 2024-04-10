b93b1a041c940ed5d275c2322ca4b43d450a659c
Chris <10873044@uvu.edu>
2024-04-10 10:30:11 -0600

    exam 1 redo

--- Assignments/Assignment_9/Assignment_9.Rproj/Assignment_9_submit.R
+++ Assignments/Assignment_9/Assignment_9.Rproj/Assignment_9_submit.R
@@ -6,30 +6,33 @@ library(ggpubr)
 library(pacman)
 library(knitr)
 library(bookdown)
+library(rmarkdown)
 pacman::p_load('knitr', 'lme4', 'lmerTest', 'effects', 'dplyr', 'plotrix', 'car', 'bookdown')
 
-#Read inggpubr#Read in data
+##Read in data
 dat <- rmodelrdat <- read_csv("GradSchool_Admissions.csv")
 
 
 #looking at comparisons
-dat %>% 
+fig_0 <- dat %>% 
   select(admit,gre,gpa,rank) %>% 
   ggpairs()
 
+
 #sticking with gre and gpa
 #graphs
-dat %>% 
+fig_1 <- dat %>% 
   ggplot(aes(x=gre,y=gpa))+
   geom_point()+
   facet_grid(admit~rank)+
   theme_minimal()
+  fig_1
   
   ggpubr::ggqqplot(dat$gre)
   
 
 #models
-names(dat)
+
 mod1 <- glm(data=dat,
             formula=gre~gpa+admit)
 
@@ -55,7 +58,7 @@ comps %>%
   plot()
 
 #predictions,doesnt seem to be following the trend as hard as an incline
-dat %>% 
+fig_2 <- dat %>% 
   gather_predictions(mod1,mod2,mod3) %>% 
   ggplot(aes(x=gre,y=pred,color=model))+
   geom_segment(aes(x=0,y=0,xend=800,yend=650),linetype=2,color="black",alpha=.5)+
@@ -65,6 +68,7 @@ dat %>%
   scale_color_viridis_d()+
   labs(title = "Predictions vs Observations",
        subtitle="Dashed line indicates perfect overlap between observed values and predctions")
+  fig_2
   
 set.seed(123)
 training_samples <- caret::createDataPartition(seq_along(dat$gre),
@@ -82,7 +86,7 @@ mod1 <- glm(data=train_data,
             formula=mod1_formula)
 
 
-gather_predictions(test_data,mod2,mod1) %>% 
+fig_3 <- gather_predictions(test_data,mod2,mod1) %>% 
   ggplot(aes(x=gre,y=pred,color=model))+
   geom_segment(aes(x=0,y=0,xend=800,yend=650),linetype=2,color="black",alpha=.5)+
   geom_smooth(method="lm",se=FALSE)+
@@ -92,4 +96,5 @@ theme_minimal()+
   labs(title = "Predictions vs Observations",
        subtitle="Dashed line indicates perfect overlap between observed values and predctions")
 
+fig_3
 mod2 %>% model_parameters()
